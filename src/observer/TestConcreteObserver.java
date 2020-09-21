9c220203a4a018f155ef28d227229864714e32a6
ikerexxe <ikerpedrosam@gmail.com>
2020-09-21 20:07:55 +0200

    Observer: increment unit test coverage

--- src/observer/TestConcreteObserver.java
+++ src/observer/TestConcreteObserver.java
@@ -5,27 +5,49 @@ import static org.junit.jupiter.api.Assertions.*;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class TestConcreteObserver {
 	
+	Subject subject;
+	List<Observer> observers;
 	static int MAX_OBSERVERS = 2;
-
-	@Test
-	void whenObserversAreRegistered_thenObserversAreNotified() {
-		Subject subject = new ConcreteSubject();
-		List<Observer> observers = new ArrayList<>();
+	
+	@BeforeEach
+	void givenSomeObserverAreRegistered() {
+		subject = new ConcreteSubject();
+		observers = new ArrayList<>();
 		
 		for(int i = 0; i <= MAX_OBSERVERS; i++) {
 			observers.add(new ConcreteObserver(i));
 			subject.registerObserver(observers.get(i));
 		}
-		
+	}
+
+	@Test
+	void whenSubjectChanges_thenObserversAreNotified() {
 		subject.notifyObservers();
 		
 		for(int i = 0; i <= MAX_OBSERVERS; i++) {
 			boolean result = observers.get(i).isNotified();
 			assertEquals(true, result);
+		}
+	}
+	
+	@Test
+	void whenLastObserverIsRemovedAndSubjectChanges_thenObserversZeroAndOneAreNotified() {
+		subject.removeObserver(observers.get(MAX_OBSERVERS));
+		subject.notifyObservers();
+		
+		for(int i = 0; i <= MAX_OBSERVERS; i++) {
+			boolean result = observers.get(i).isNotified();
+			
+			if (i != MAX_OBSERVERS) {
+				assertEquals(true, result);
+			} else {
+				assertEquals(false, result);
+			}
 		}
 	}
 
